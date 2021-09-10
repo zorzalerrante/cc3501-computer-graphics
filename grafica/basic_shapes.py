f3cfb7c6af7f4a7710e99ea997d99fa96e863d74
ivan <isipiran@pucp.edu.pe>
2021-09-10 10:55:40 -0500

    new resources

--- grafica/basic_shapes.py
+++ grafica/basic_shapes.py
@@ -3,6 +3,11 @@
 """Vertices and indices for a variety of simple shapes"""
 
 import math
+import numpy as np
+import sys
+import os.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from grafica.assets_path import getAssetPath
 
 __author__ = "Daniel Calderon"
 __license__ = "MIT"
@@ -231,6 +236,98 @@ def createRainbowCube():
 
     return Shape(vertices, indices)
 
+def readOFF(filename, color):
+    vertices = []
+    normals= []
+    faces = []
+
+    with open(filename, 'r') as file:
+        line = file.readline().strip()
+        assert line=="OFF"
+
+        line = file.readline().strip()
+        aux = line.split(' ')
+
+        numVertices = int(aux[0])
+        numFaces = int(aux[1])
+
+        for i in range(numVertices):
+            aux = file.readline().strip().split(' ')
+            vertices += [float(coord) for coord in aux[0:]]
+        
+        vertices = np.asarray(vertices)
+        vertices = np.reshape(vertices, (numVertices, 3))
+        #print(f'Vertices shape: {vertices.shape}')
+
+        normals = np.zeros((numVertices,3), dtype=np.float32)
+        #print(f'Normals shape: {normals.shape}')
+
+        for i in range(numFaces):
+            aux = file.readline().strip().split(' ')
+            aux = [int(index) for index in aux[0:]]
+            faces += [aux[1:]]
+            
+            vecA = [vertices[aux[2]][0] - vertices[aux[1]][0], vertices[aux[2]][1] - vertices[aux[1]][1], vertices[aux[2]][2] - vertices[aux[1]][2]]
+            vecB = [vertices[aux[3]][0] - vertices[aux[2]][0], vertices[aux[3]][1] - vertices[aux[2]][1], vertices[aux[3]][2] - vertices[aux[2]][2]]
+
+            res = np.cross(vecA, vecB)
+            normals[aux[1]][0] += res[0]  
+            normals[aux[1]][1] += res[1]  
+            normals[aux[1]][2] += res[2]  
+
+            normals[aux[2]][0] += res[0]  
+            normals[aux[2]][1] += res[1]  
+            normals[aux[2]][2] += res[2]  
+
+            normals[aux[3]][0] += res[0]  
+            normals[aux[3]][1] += res[1]  
+            normals[aux[3]][2] += res[2]  
+        #print(faces)
+        norms = np.linalg.norm(normals,axis=1)
+        normals = normals/norms[:,None]
+
+        color = np.asarray(color)
+        color = np.tile(color, (numVertices, 1))
+
+        vertexData = np.concatenate((vertices, color), axis=1)
+        vertexData = np.concatenate((vertexData, normals), axis=1)
+
+        #print(vertexData.shape)
+
+        indices = []
+        vertexDataF = []
+        index = 0
+
+        for face in faces:
+            vertex = vertexData[face[0],:]
+            vertexDataF += vertex.tolist()
+            vertex = vertexData[face[1],:]
+            vertexDataF += vertex.tolist()
+            vertex = vertexData[face[2],:]
+            vertexDataF += vertex.tolist()
+            
+            indices += [index, index + 1, index + 2]
+            index += 3        
+
+
+
+        return Shape(vertexDataF, indices)
+
+def createColorCubeTarea2(r,g,b):
+
+    return readOFF(getAssetPath('cube.off'), (r, g, b))
+
+def createColorSphereTarea2(r,g,b):
+
+    return readOFF(getAssetPath('sphere.off'), (r, g, b))
+
+def createColorCylinderTarea2(r,g,b):
+
+    return readOFF(getAssetPath('cylinder.off'), (r, g, b))
+
+def createColorConeTarea2(r,g,b):
+
+    return readOFF(getAssetPath('cone.off'), (r, g, b))
 
 def createColorCube(r, g, b):
 
