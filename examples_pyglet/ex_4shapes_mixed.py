e4feee85bcce8a69dd539c41a1af29f50124f68f
AlasAltum <alonso.utreras@ug.uchile.cl>
2023-01-02 22:59:21 -0400

    Created another version of shaders more static and using inheritance. Created a mixed version of the example of 4 shapes. Created a higher order version of GPUShape

--- examples_pyglet/ex_4shapes_mixed.py
+++ examples_pyglet/ex_4shapes_mixed.py
@@ -11,7 +11,8 @@ import os.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from grafica import basic_shapes as bs
 from grafica import easy_shaders as es
-
+from grafica import transformations as tr
+from shapes_utils import HighLevelGPUShape, createGPUShape
 
 class Controller(pyglet.window.Window):
 
@@ -19,33 +20,23 @@ class Controller(pyglet.window.Window):
         super().__init__(width, height, title)
         self.total_time = 0.0
         self.fillPolygon = True
-
+        self.pipeline = None
+        self.repeats = 0
 
 # We will use the global controller as communication with the callback function
 WIDTH, HEIGHT = 1280, 800
 controller = Controller(width=WIDTH, height=HEIGHT)
-
 # Setting up the clear screen color
-pyglet.gl.glClearColor(0.15, 0.15, 0.15, 1.0)
+glClearColor(0.15, 0.15, 0.15, 1.0)
 
 # Setting the model (data of our code)
-   # Creating our shader program and telling OpenGL to use it
+# Creating our shader program and telling OpenGL to use it
 pipeline = es.SimpleTransformShaderProgram()
-pyglet.gl.glUseProgram(pipeline.shaderProgram)
+controller.pipeline = pipeline
+glUseProgram(pipeline.shaderProgram)
 
-# Setting up the clear screen color
-pyglet.gl.glClearColor(0.15, 0.15, 0.15, 1.0)
-
-
-
-
-gpuTriangle = createGPUShape(pipeline, bs.createRainbowTriangle())
-
-shapeQuad = bs.createRainbowQuad()
-gpuQuad = es.GPUShape().initBuffers()
-pipeline.setupVAO(gpuQuad)
-gpuQuad.fillBuffers(shapeQuad.vertices, shapeQuad.indices, pyglet.gl.GL_STATIC_DRAW)
-
+gpuTriangle = HighLevelGPUShape(pipeline, bs.createRainbowTriangle())
+gpuQuad = HighLevelGPUShape(pipeline, bs.createRainbowQuad())
 
 # What happens when the user presses these keys
 @controller.event
@@ -55,20 +46,54 @@ def on_key_press(symbol, modifiers):
     elif symbol == pyglet.window.key.ESCAPE:
         controller.close()
 
+# Each time update is called, on_draw is called again
+# That is why it is better to draw and update each one in a separated function
+# We could also create 2 different gpuQuads and different transform for each
+# one, but this would use more memory
+def update(dt, controller):
+    controller.total_time += dt
+
+# Try to call this function 60 times per second
+pyglet.clock.schedule(update, controller)
+
+# This function will be executed approximately 60 times per second
+# dt is the time between the last time it was executed and now
+def draw_translated_quad(controller: Controller):
+    gpuQuad.scale = tr.uniformScale(0.8)
+    gpuQuad.translation = tr.translate(0.5, 0.5, 0.0)
+    gpuQuad.draw(controller.pipeline)
+
+def draw_scalating_quad(controller: Controller):
+    gpuQuad.scale = tr.uniformScale(0.5 * sin(controller.total_time))
+    gpuQuad.translation = tr.translate(-0.5, -0.5, 0.0)
+    gpuQuad.draw(controller.pipeline)
+
+# This function will be executed approximately 60 times per second
+# dt is the time between the last time it was executed and now
+def draw_shearing_triangle(controller: Controller):
+    gpuTriangle._transform = tr.matmul([
+        tr.translate(0.5, -0.5, 0),
+        tr.shearing(0.3 * sin(controller.total_time), 0, 0, 0, 0, 0),
+        tr.uniformScale(0.7)
+    ])
+    gpuTriangle.draw(controller.pipeline)
+
+def draw_rotating_triangle(controller: Controller):
+    gpuTriangle._transform = tr.identity()  # Make the transform neutral again
+    gpuTriangle.rotation = tr.rotationZ(controller.total_time)
+    gpuTriangle.translation = tr.translate(-0.5, 0.5, 0.0)
+    gpuTriangle.scale = tr.uniformScale(0.3)
+    gpuTriangle.draw(controller.pipeline)
+
+
 
 @controller.event
 def on_draw():
     controller.clear()
+    draw_translated_quad(controller)
+    draw_scalating_quad(controller)
+    draw_shearing_triangle(controller)
+    draw_rotating_triangle(controller)
 
-
-# # This function will be executed approximately 60 times per second
-# # dt is the time between the last time it was executed and now
-# def update_figures(dt: float, controller: Controller):
-#     controller.total_time += dt
-#     shape_quad1.rotation = 20.0 * controller.total_time
-#     shape_triangle2.position = 100.0 + 100.0 * cos(controller.total_time), 100.0 + 100.0 * sin(controller.total_time)
-#     shape_quad2.scale = 1.0 + 0.5 * sin(controller.total_time)
-
-# pyglet.clock.schedule(update_figures, controller)
 # Set the view
 pyglet.app.run()
--- examples_pyglet/shapes_utils.py
+++ examples_pyglet/shapes_utils.py
@@ -3,18 +3,64 @@ from OpenGL.GL import *
 import sys
 import os.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import grafica.basic_shapes as bs
-import grafica.easy_shaders as es
+import grafica.transformations as tr
+from grafica.easy_shaders import GPUShape 
+from grafica.basic_shapes import Shape
+
+# Creating shapes on GPU memory
+def createGPUShape(pipeline, shape):
+    gpuShape = GPUShape().initBuffers()
+    pipeline.setupVAO(gpuShape)
+    gpuShape.fillBuffers(shape.vertices, shape.indices, GL_STATIC_DRAW)
+    return gpuShape
 
 
 class HighLevelGPUShape:
 
-    def __init__(self, pipeline, GPUShape: es.GPUShape):
+    def __init__(self, pipeline, shape: Shape):
+        self._rotation = tr.identity()
+        self._translation = tr.identity()
+        self._scale = tr.identity()
+        self._transform = tr.identity()
+        self._GPUShape = createGPUShape(pipeline, shape)
 
+    def _update_transform(self):
+        self._transform = self._translation @ self._rotation @ self._scale
+    
+    @property
+    def translation(self):
+        return self._translation
 
-# Creating shapes on GPU memory
-def createGPUShape(pipeline, shape):
-    gpuShape = es.GPUShape().initBuffers()
-    pipeline.setupVAO(gpuShape)
-    gpuShape.fillBuffers(shape.vertices, shape.indices, GL_STATIC_DRAW)
-    return gpuShape
+    @translation.setter
+    def translation(self, value):
+        self._translation = value
+        self._update_transform()
+
+    @property
+    def scale(self):
+        return self._scale
+
+    @scale.setter
+    def scale(self, value):
+        self._scale = value
+        self._update_transform()
+
+    @property
+    def rotation(self):
+        return self._rotation
+
+    @rotation.setter
+    def rotation(self, value):
+        self._rotation = value
+        self._update_transform()
+
+    def draw(self, pipeline):
+        # TODO: assert that pipeline has transform uniform
+        glUniformMatrix4fv(
+            glGetUniformLocation(pipeline.shaderProgram, "transform"),
+            1,
+            GL_TRUE,
+            self._transform,
+        )
+        pipeline.drawCall(self._GPUShape)
+
